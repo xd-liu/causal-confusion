a5a1a9f6317e8f080ed6988db5dc36415aae033b
XDong18 <liuxd1215@gmail.com>
2022-11-19 19:39:22 -0500

    debug

--- ccil/environments/mountain_car.py
+++ ccil/environments/mountain_car.py
@@ -82,6 +82,8 @@ class MountainCarStateEncoder:
             prev_action = trajectory.actions[-1]
         else:
             prev_action = np.atleast_1d(np.random.randint(0, 3))
+        
+        print("\n State encoder:", state, prev_action)
         x = np.concatenate([state, prev_action])
         return x
 
